f52d0eeff131075dfd0f820f22c650cd85eed733
Diyar Sari <diyarsr@outlook.com>
2025-11-27 17:28:55 +0300

    script update

---
+++ script.js
@@ -0,0 +1,18 @@
+
+    const todoInput= document.getElementById("todo_input")
+    const todoList = document.getElementById("todo_list")
+    const emptyText= document.getElementById("empty_text")
+    const buttonAdd= document.getElementById("button_add")
+    const buttonUpdate = document.getElementById("button_update")
+    const buttonDelete = document.getElementById("button_delete")
+
+    function update_empty_message(){
+        if(todoList.children.length === 0){
+            emptyText.style.display = "block";
+        }
+        else {
+            emptyText.style.display = "none"
+        }
+    }
+
+    
